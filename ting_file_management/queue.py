d224a5dd3cb2f9b2337568079e766dfb429387c4
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-13 16:53:18 -0300

    implementação da função queue.py

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = []  # inicializando uma array
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)  # o append adiciona ao fim da fila
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)  # remove o primeiro da fila (índice 0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and index < len(self.queue):
+            return self.queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")
